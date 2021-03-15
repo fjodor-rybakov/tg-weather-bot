6d1384ba09b04726f06375e65e1ad4defa3a9bbb
fjodor-rybakov <fedryb@mail.ru>
2021-03-15 21:41:30 +0300

    inti project

---
+++ src/main/java/com/tgweatherbot/TgWeatherBotApplication.java
@@ -0,0 +1,13 @@
+package com.tgweatherbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgWeatherBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TgWeatherBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tgweatherbot/TgWeatherBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgweatherbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgWeatherBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
